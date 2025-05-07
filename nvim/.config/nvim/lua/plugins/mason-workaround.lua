c02a7234c6e1245817fa50bfc818e62d1d83f89e
siddgodev <siddcodes24@gmail.com>
2025-05-07 08:18:16 -0700

    config: pinning Mason version to prevent breaking changes

---
+++ nvim/.config/nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+-- TODO: Should remove this file when either Lazy supports new versions of Mason or a fix is integrated
+return {
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+}
